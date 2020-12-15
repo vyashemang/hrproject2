5ee29e6d7b375bfd8393303da9b9c4aeac506237
Hemang Vyas <vyashemang563@gmail.com>
2020-12-15 11:01:58 +0530

    Add Bean classes

--- src/main/java/com/example/erp/bean/Department.java
+++ src/main/java/com/example/erp/bean/Department.java
@@ -1,8 +1,51 @@
 package com.example.erp.bean;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "Department")
 public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer dept_id;
 
+    @Column(nullable = false)
+    private String dname;
+
+    @Column(nullable = false)
+    private Integer capacity;
+
+    public Department(Integer dept_id, String dname, Integer capacity) {
+        this.dept_id = dept_id;
+        this.dname = dname;
+        this.capacity = capacity;
+    }
+
+    public Department() {
+
+    }
+
+    public Integer getDept_id() {
+        return dept_id;
+    }
+
+    public void setDept_id(Integer dept_id) {
+        this.dept_id = dept_id;
+    }
+
+    public String getDname() {
+        return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
 }
--- src/main/java/com/example/erp/bean/Employee.java
+++ src/main/java/com/example/erp/bean/Employee.java
@@ -1,4 +1,83 @@
 package com.example.erp.bean;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Employee")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer emp_id;
+
+    @Column(nullable = false)
+    private String first_name;
+
+    private String last_name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String title;
+    private String photo;
+
+    // ADD FOREIGN KEY CONTRAINTS.
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dept_id", insertable = false)
+    private Department dept_id;
+
+    public Integer getEmp_id() {
+        return emp_id;
+    }
+
+    public void setEmp_id(Integer emp_id) {
+        this.emp_id = emp_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Department getDept_id() {
+        return dept_id;
+    }
+
+    public void setDept_id(Department dept_id) {
+        this.dept_id = dept_id;
+    }
 }
